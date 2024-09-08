3043d53d85ac2340c3a5252f027b2ccaf877900c
Ivan Ermolaev <eriv31@yandex.ru>
2024-09-08 17:03:18 +0300

    adding_index.js

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Проектная работа Mesto
 
+https://github.com/ErIv31/mesto-project-ff.git
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,27 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
+const cardList = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
+function createCard(cardData, deleteCard) {
+  const currentCard = cardTemplate.cloneNode(true);
+  currentCard.querySelector(".card__title").textContent = cardData.name;
+  currentCard.querySelector(".card__image").src = cardData.link;
+  currentCard
+    .querySelector(".card__delete-button")
+    .addEventListener("click", deleteCard);
+
+  return currentCard;
+}
 
 // @todo: Функция удаления карточки
+const deleteCard = (event) => {
+  event.target.closest(".card").remove();
+};
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach((card) => {
+  cardList.append(createCard(card, deleteCard));
+});
