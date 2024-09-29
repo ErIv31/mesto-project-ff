b88f2cdaecf400cc76941c4b58a03f57e1df3209
Ivan Ermolaev <eriv31@yandex.ru>
2024-09-29 20:24:17 +0300

    changing card.js

--- src/scripts/components/card.js
+++ src/scripts/components/card.js
@@ -2,16 +2,23 @@
 const cardTemplate = document.querySelector("#card-template").content;
 
 // @todo: Функция создания карточки
-function createCard(cardData, deleteCard) {
-  const currentCard = cardTemplate.cloneNode(true);
+function createCard(cardData, deleteCard, likeCard, zoomCard) {
+  const currentCard = cardTemplate.querySelector(".card").cloneNode(true);
   const cardImage = currentCard.querySelector(".card__image");
 
   currentCard.querySelector(".card__title").textContent = cardData.name;
   cardImage.src = cardData.link;
   cardImage.alt = cardData.name;
+
   currentCard
     .querySelector(".card__delete-button")
     .addEventListener("click", deleteCard);
+  currentCard
+    .querySelector(".card__like-button")
+    .addEventListener("click", likeCard);
+  cardImage.addEventListener("click", () =>
+    zoomCard(cardData.link, cardData.name)
+  );
 
   return currentCard;
 }
@@ -23,5 +30,5 @@ function deleteCard(event) {
 
 // @todo: Функция лайка карточки
 function likeCard(event) {
-  event.target.classList.toggle('card__like-button_is-active');
+  event.target.classList.toggle("card__like-button_is-active");
 }
