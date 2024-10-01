3a90348b9d59cfac496e9c608a280b0c9993c389
Ivan Ermolaev <eriv31@yandex.ru>
2024-10-02 00:59:12 +0300

    changing index.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,13 +1,12 @@
 import '../pages/index.css';
-
 import { initialCards } from './components/cards';
-
 import { createCard, deleteCard, likeCard } from './components/card';
-
 import { openModal, closeModal } from './components/modal';
 
 // @todo: DOM узлы
-const cardList = document.querySelector(".places__list");
+const cardList = document.querySelector(".places__list"),
+  profile = document.querySelector('.profile'),
+  ;
 
 
 
