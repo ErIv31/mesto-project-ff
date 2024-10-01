87f9974d1d268ad2e43df22e31552bd310ac4bf7
Ermolaev Ivan <eriv31@yandex.ru>
2024-10-01 16:22:15 +0300

    changing modal.js

--- src/scripts/components/modal.js
+++ src/scripts/components/modal.js
@@ -8,6 +8,8 @@ export function openModal(popup) {
 export function closeModal(popup) {
   popup.classList.remove("popup_is-opened");
   document.removeEventListener("keydown", closeModalEsc);
+  document.removeEventListener('click', closeModalOverlay);
+  popup.querySelector('.popup__close').addEventListener('click', closeModalCloseButton);
 }
 
 // @todo: Функция закрытия модального окна по кнопке Esc
@@ -17,3 +19,16 @@ function closeModalEsc(event) {
   }
 }
 
+// @todo: Функция закрытия модального окна по крестику в модальном окне
+function closeModalCloseButton(event) {
+  closeModal(event.target.closest('.popup'));
+}
+
+// @todo: Функция закрытия модального окна по оверлею
+function closeModalOverlay(e) {
+  document.querySelector('.popup').addEventListener('click', (event) => {
+    if (event.target === document.querySelector('.popup')) {
+      closeModal(e.target.closest('.popup'));
+    }
+  })
+}
