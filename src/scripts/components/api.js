84d9af197d1a895a45321507258b28b605de1789
Ivan Ermolaev <eriv31@yandex.ru>
2024-10-27 17:24:30 +0300

    changing api.js

--- src/scripts/components/api.js
+++ src/scripts/components/api.js
@@ -1,15 +1,82 @@
+// Конфигурация пользователя
 const config = {
-  baseUrl: 'https://nomoreparties.co/v1/wff-cohort-24',
+  baseUrl: "https://nomoreparties.co/v1/wff-cohort-24",
   headers: {
-    authorization: 'a60c934d-c4f1-48bc-9c8f-41700db42dbf',
-    'Content-Type': 'application/json'
+    authorization: "a60c934d-c4f1-48bc-9c8f-41700db42dbf",
+    "Content-Type": "application/json",
+  },
+};
+
+// Проверка статуса запроса серверу
+function handleResponse(res) {
+  if (res.ok) {
+    return res.json();
+  } else {
+    return Promise.reject(`Ошибка: ${res.status}`);
   }
 }
 
-function handleResponse(res) {
-  if (res.ok) {
-    return res.json()
-  } else {
-    return Promise.reject(`Ошибка: ${res.status}`)
-  }
+// Получение профиля пользователя
+export function getUserProfile() {
+  return fetch(`${config.baseUrl}/users/me`, {
+    headers: config.headers,
+  }).then((res) => handleResponse(res));
+}
+
+// Редактирование профиля пользователя
+export function editUsertProfile(name, about) {
+  return fetch(`${config.baseUrl}/users/me`, {
+    method: "PATCH",
+    headers: config.headers,
+    body: JSON.stringify({ name, about }),
+  }).then((res) => handleResponse(res));
+}
+
+// Редактирование аватара пользователя
+export function editUserAvatar(avatar) {
+  return fetch(`${config.baseUrl}/users/me/avatar`, {
+    method: "PATCH",
+    headers: config.headers,
+    body: JSON.stringify({ avatar }),
+  }).then((res) => handleResponse(res));
+}
+
+// Получение массива карточек
+export function getInitialCards() {
+  return fetch(`${config.baseUrl}/cards`, {
+    headers: config.headers,
+  }).then((res) => handleResponse(res));
+}
+
+// Добавление новой карточки
+export function addNewCard(name, link) {
+  return fetch(`${config.baseUrl}/cards`, {
+    method: "POST",
+    headers: config.headers,
+    body: JSON.stringify({ name, link }),
+  }).then((res) => handleResponse(res));
+}
+
+// Удаление карточки
+export function removeCard(id) {
+  return fetch(`${config.baseUrl}/cards/${id}`, {
+    method: "DELETE",
+    headers: config.headers,
+  }).then((res) => handleResponse(res));
+}
+
+// Добавление лайка карточке
+export function addLike(id) {
+  return fetch(`${config.baseUrl}/cards/likes/${id}`, {
+    method: "PUT",
+    headers: config.headers,
+  }).then((res) => handleResponse(res));
+}
+
+// Удаление лайка у карточки
+export function removeLike(id) {
+  return fetch(`${config.baseUrl}/cards/likes/${id}`, {
+    method: "DELETE",
+    headers: config.headers,
+  }).then((res) => handleResponse(res));
 }
