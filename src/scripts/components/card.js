74e3e4ab9404ec2ab7c8c3078476dae0b35bce43
Ivan Ermolaev <eriv31@yandex.ru>
2024-10-27 21:48:17 +0300

    change card.js

--- src/scripts/components/card.js
+++ src/scripts/components/card.js
@@ -1,21 +1,42 @@
-// @todo: Темплейт карточки
+import { addLike, removeLike } from "./api.js";
+
+//Темплейт карточки
 const cardTemplate = document.querySelector("#card-template").content;
 
-// @todo: Функция создания карточки
-export function createCard(cardData, deleteCard, likeCard, zoomCard) {
+//Функция создания карточки
+export function createCard(cardData, userID, deleteCard, likeCard, zoomCard) {
   const currentCard = cardTemplate.querySelector(".card").cloneNode(true);
-  const cardImage = currentCard.querySelector(".card__image");
 
-  currentCard.querySelector(".card__title").textContent = cardData.name;
+  const cardImage = currentCard.querySelector(".card__image");
+  const cardTitle = currentCard.querySelector(".card__title");
+  const cardDeleteButton = currentCard.querySelector(".card__delete-button");
+  const cardLikeButton = currentCard.querySelector(".card__like-button");
+  const likeCount = currentCard.querySelector(".card__like-counts");
+
+  cardTitle.textContent = cardData.name;
   cardImage.src = cardData.link;
   cardImage.alt = cardData.name;
+  likeCount.textContent = cardData.likes.length;
 
-  currentCard
-    .querySelector(".card__delete-button")
-    .addEventListener("click", deleteCard);
-  currentCard
-    .querySelector(".card__like-button")
-    .addEventListener("click", likeCard);
+  if (cardData.owner._id == userID) {
+    cardDeleteButton.addEventListener("click", () => {
+      deleteCard(currentCard, cardData._id);
+    });
+  } else {
+    cardDeleteButton.setAttribute("style", `display: none;`);
+  }
+
+  if (
+    cardData.likes.some((likedUser) => {
+      likedUser._id == userID;
+    })
+  ) {
+    cardLikeButton.classList.add("card__like-button_is-active");
+  }
+
+  cardLikeButton.addEventListener("click", (event) =>
+    likeCard(event.target, likeCount, cardData._id)
+  );
   cardImage.addEventListener("click", () =>
     zoomCard(cardData.link, cardData.name)
   );
@@ -23,12 +44,30 @@ export function createCard(cardData, deleteCard, likeCard, zoomCard) {
   return currentCard;
 }
 
-// @todo: Функция удаления карточки
-export function deleteCard(event) {
-  event.target.closest(".card").remove();
+//Функция удаления карточки
+export function deleteCard(card) {
+  card.remove();
 }
 
-// @todo: Функция лайка карточки
-export function likeCard(event) {
-  event.target.classList.toggle("card__like-button_is-active");
+//Функция лайка карточки
+export function likeCard(event, cardId, likeCount) {
+  if (event.target.classList.contains("card__like-button_is-active")) {
+    removeLike(cardId)
+      .then((card) => {
+        likeCount.textContent = card.likes.length;
+        event.target.classList.remove("card__like-button_is-active");
+      })
+      .catch((err) => {
+        console.log(`Ошибка: ${err}`);
+      });
+  } else {
+    addLike(cardId)
+      .then((card) => {
+        likeCount.textContent = card.likes.length;
+        event.target.classList.add("card__like-button_is-active");
+      })
+      .catch((err) => {
+        console.log(`Ошибка: ${err}`);
+      });
+  }
 }
